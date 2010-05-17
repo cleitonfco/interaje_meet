879d6ffa11292acac1907b711219f79e87bbb981
cyrus <cyruscavalcante@gmail.com>
2010-05-17 15:56:45 -0300

    submeter inscricao

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.with_options :controller => "users" do |user|
     user.subscribe  "/inscricao", :action => "subscribe", :conditions => { :method => :get }
-    user.subscribe_event  "/inscricao/:envent/:user/:token", :action => "subscribe_event", :conditions => { :method => :get }
+    user.subscribe_event  "/inscricao_evento/:envent/:user/:token", :action => "subscribe_event", :conditions => { :method => :get }
     user.users      "/", :action => "index", :conditions => { :method => :get }
     user.connect    "/", :action => "create", :conditions => { :method => :post }
   end
