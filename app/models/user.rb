18fd6e9a76268d5da76573a796b293c85d3b55ba
Cleiton Francisco <cleitonfco@gmail.com>
2010-05-17 14:12:37 -0300

    Gera código automaticamente

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,18 @@ class User < ActiveRecord::Base
   named_scope :twitters, lambda { |event| 
     { :conditions => ["(twitter is not NULL AND twitter <> '') and event_id = ?", event.id], :order => 'created_at DESC' }
   }
-  
+
   validates_presence_of :name, :email
-  
+
+  before_save :generate_code
+
   def humanize_site
     "http://#{site.downcase.gsub("http://", "")}"
   end
+
+  protected
+    def generate_code
+      code = UUID.generate.split('-')
+      self.code = "#{code[0]}#{code[1]}"
+    end
 end
